5675a2cbb838fd76060e1876379dc2b9d203b95f
Dmitry Frishbuter <dmitry.frishbuter@rosberry.com>
2017-09-25 15:44:39 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -o pipefail
 
-xcodebuild \
-  -project "$PROJECT" \
-  -scheme "$IOS_FRAMEWORK_SCHEME" \
-  -destination "$DESTINATION" \
-  -enableCodeCoverage YES build test | xcpretty -c --test --color
+xcodebuild -project "$PROJECT" -scheme "$IOS_FRAMEWORK_SCHEME" -destination "$DESTINATION" -enableCodeCoverage YES build test | xcpretty -c --test --color
