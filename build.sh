f18e22359cdf3fe8bdb894a779059545d0d16379
Dmitry Frishbuter <dmitry.frishbuter@rosberry.com>
2017-09-25 15:11:42 +0600

    Update and rename script.sh to build.sh
    [ci skip]

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+- set -o pipefail
+
+- > 
+    xcodebuild
+    -project "$PROJECT"
+    -scheme "$IOS_FRAMEWORK_SCHEME"
+    -destination "$DESTINATION"
+    -enableCodeCoverage YES build test | xcpretty -c --test --color
--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -euxo pipefail
-
-xcodebuild test -project Tribulus.xcodeproj -scheme Tribulus -destination 'platform=iOS Simulator,OS=10.3.1,name=iPhone 7'
