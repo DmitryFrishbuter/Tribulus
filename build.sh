f083377fc948a8414d381b72b1215c25671cbe4d
Dmitry Frishbuter <dmitry.frishbuter@rosberry.com>
2017-09-25 16:10:17 +0600

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -o pipefail
-
-xcodebuild \
-  -project "$PROJECT" \
-  -scheme "$IOS_FRAMEWORK_SCHEME" \
-  -destination "$DESTINATION" \
-  -enableCodeCoverage YES \
-  build test | xcpretty -c --test --color
